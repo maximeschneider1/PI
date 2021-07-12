7897f108346179eb7188fb6a4845f891c80ed7d8
Maxime Schneider <m_schneider@etu-webschoolfactory.fr>
2021-07-12 17:30:24 +0200

    missing go function

--- go-server/main.go
+++ go-server/main.go
@@ -10,6 +10,35 @@ func main() {
 	http.HandleFunc("/", handler.HandleMain)
 	http.HandleFunc("/LogToLeboncoin", handler.HandleGoogleLogin)
 
+	h := `
+<html>
+ <body>
+  <a name="0"> text </a>
+  <a name="1"> abc </a>
+  <a name="2"> def ghi jkl </a>
+  <a name="3"> vert </a>
+  <a name="4"> Some text </a>
+ </body>
+</html>`
+
+        pattern := "vert"
+
+        doc, err := goquery.NewDocumentFromReader(strings.NewReader(h))
+        if err != nil {
+                panic(err)
+        }
+
+        doc.Find("a").Each(func(i int, s *goquery.Selection) {
+                if strings.TrimSpace(s.Text()) == pattern {
+                        name, ok := s.Attr("bike")
+                        if ok {
+                                fmt.Println(bike)
+                        }
+                }
+        })
+
+}
+
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
